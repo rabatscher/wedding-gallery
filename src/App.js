ea751a5ba24b92284ddc3cb8cb688aa9696582ec
rabatscher <p.rabatscher@hotmail.com>
2023-09-17 13:47:49 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./components/Login";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid place-content-center h-screen">
+      <h1 className="text-3xl underline">Hellow World!1</h1>
+      <Login />
     </div>
   );
 }
-
-export default App;
