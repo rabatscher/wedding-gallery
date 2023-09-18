1a645088b49b65056b1628e4f919af9448ea7afc
rabatscher <p.rabatscher@hotmail.com>
2023-09-18 17:19:58 +0200

    Update Albums.js

--- src/components/Albums.js
+++ src/components/Albums.js
@@ -4,7 +4,7 @@ import AlbumItem from "./AlbumItem";
 export default function Albums({ onSetAlbum }) {
   return (
     // WRAPPER
-    <div className="flex flex-col grow gap-0 w-full ">
+    <div className="flex flex-col grow gap-0 w-full h-screen">
       <AlbumItem
         text="Schloss Heidegg"
         source="albums/album_001.jpg"
