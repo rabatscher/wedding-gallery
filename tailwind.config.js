7f647689cdfbe035628982ec2179b50aad0cdaef
rabatscher <p.rabatscher@hotmail.com>
2023-09-18 15:52:34 +0200

    §

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        honey: ['"hello honey - Personal Use"'],
+        honey: ['"hellohoney"'],
         book: ['"Book Antiqua"'],
       },
       animation: {
