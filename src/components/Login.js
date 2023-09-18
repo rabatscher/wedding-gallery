1bd5b4ee428a73679be0c21242ea92eba595f738
rabatscher <p.rabatscher@hotmail.com>
2023-09-18 13:19:58 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Login({ onChangePassword, password }) {
   return (
-    <div className="grid place-content-center h-screen">
+    <div className="grid place-content-center h-full">
       <div className="text-center content-center justify-center flex items-center flex-col">
         <h2 className="text-6xl lg:text-8xl pb-12 font-honey">Bildergalerie</h2>
         <img
